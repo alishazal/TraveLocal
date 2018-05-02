f59aadf78341c6e9e0d3a2b9b25453b7abaa4d2f
Ali Shazal <alishazal@Alis-MacBook-Air.local>
2018-05-02 20:15:22 +0400

    uploading files

---
+++ cgi-bin/formSubmission.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3.2
+print("Content-Type: text/html \n")
+
+#Initializing html as function for ease
+def htmlhead():
+	print("<!DOCTYPE HTML><html><head><title>My output</title><link rel='stylesheet' type='text/css' href='../style.css'></head><body>")
+
+
+htmlhead()
+
+#Importing cgi for the form
+import cgi
+
+data=cgi.FieldStorage()
+
+#Variables for the form
+name = data['name'].value
+email = data['email'].value
+time = data['category'].value
+area = data['area'].value
+
+#Printing out message for the user
+print("<h1> Dear", name + ",<br/><br/> Your request has been noted. <br/> We'll email you 15 minutes before", category,"on", email +  ".</h1>")
+
+#Closing the html
+print("</body></html>")
